749c772f67e6288bcc9c14871cb1121374aa5563
Kaitlin Naughten <k.naughten@unsw.edu.au>
2017-01-13 10:44:09 +1100

    Miscellaneous bug fixes; everything runs now

--- apps/common/python/ModelRun.py
+++ apps/common/python/ModelRun.py
@@ -27,7 +27,7 @@ class ModelRun(object):
         self._replace_keywords_cice_in()
         # Check to see if roms ini- and cice ini-file have same timestamp:
         if (self._params.RESTART == True):
-            self.check_starttime()
+            self._check_starttime()
         self._run(runoption,debugoption,architecture)
         # Should check output-files to verify a successful run?
         # Output to std.out that model has finished:
@@ -250,8 +250,8 @@ class ModelRun(object):
             exit(1)
 
     def _check_starttime(self):
-        roms_ini = netCDF4.num2date(netCDF4.Dataset(self._params.RUNPATH+"/ocean_ini.nc").variables['ocean_time'][:], 
-            netCDF4.Dataset(self._params.RUNPATH+"/ocean_ini.nc").variables['ocean_time'].units)
+        roms_ini = netCDF4.num2date(netCDF4.Dataset(self._params.RUNPATH+"/ocean_rst.nc").variables['ocean_time'][:], 
+            netCDF4.Dataset(self._params.RUNPATH+"/ocean_rst.nc").variables['ocean_time'].units)
         f = open(self._params.CICERUNDIR+'/restart/ice.restart_file', 'r')
         cice_restartfile = f.readline().strip()
         cice_rst_day = netCDF4.Dataset(cice_restartfile).mday
